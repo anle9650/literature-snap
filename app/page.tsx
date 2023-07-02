86508e938768aff29a2066ba1b87d61731a0921d
anle9650 <andy.le@ucdenver.edu>
2023-07-01 22:49:22 -0600

    fetch articles

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { useState } from "react";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const [articleIds, setArticleIds] = useState<string[]>([]);
+  const router = useRouter();
 
-  const handleSearch = async (searchTerm: string) => {
-    const response = await fetch(
-      `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&format=json&term=science%5bjournal%5d+AND+${searchTerm.trim()}`
-    );
-    const data = await response.json();
-    setArticleIds(data.esearchresult.idlist);
+  const handleSearch = (searchTerm: string) => {
+    router.push(`/articles?term=${searchTerm.trim()}`);
   };
 
   return (
