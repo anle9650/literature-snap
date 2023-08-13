e41271569c5669969ba6f4745b0934ed01146425
anle9650 <andy.le@ucdenver.edu>
2023-08-12 19:20:30 -0600

    remove mock

--- app/api/articles/summarize/route.ts
+++ app/api/articles/summarize/route.ts
@@ -19,11 +19,6 @@ export const POST = async (req: Request) => {
     }),
   };
 
-  const summary =
-    "- Mutations in BRCA1 or BRCA2 genes increase the risk of breast and ovarian cancer - Treatment for these cancers involves PARP inhibitors - Inhibition of DNPH1 increases sensitivity of BRCA-deficient cells to PARP inhibitors - DNPH1 inhibition leads to replication fork collapse, DNA break formation, and apoptosis - Targeting DNPH1 may enhance the effectiveness of PARP inhibitor therapy for BRCA-deficient cancers - Cells eliminate epigenetically modified nucleotides through a two-step process involving DCTD and DNPH1 - Genomic hmdU predominantly arises through deamination of hmdCMP by DCTD - Cancers with high levels of hmdC and/or expression of CDA or DCTD may depend on DNPH1 for survival - Loss of ITPA also sensitizes cells to PARP inhibitors - Synthetic lethality following exposure to hmdU involves replication fork collapse, DSB formation, and apoptotic cell death - Loss of either PARP1 or the 53BP1-SHIELDIN pathway leads to cell death when exposed to hmdU - DNPH1 inhibition sensitizes BRCA-deficient cancer cells to PARP inhibitors and hmdU treatment - DNPH1 should be investigated as a potential druggable target";
-  const bulletPoints = summary.split("- ").slice(1);
-  return new Response(JSON.stringify(bulletPoints), { status: 200 });
-
   try {
     const response = await fetch(
       "https://api.openai.com/v1/chat/completions",
